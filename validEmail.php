3d1f62f622e849941f4dffbf2b6b65bb32f9353e
kostas o mastoras <kmitsios@agenso.gr>
2021-03-03 01:31:04 +0200

    debug 11

--- validEmail.php
+++ validEmail.php
@@ -3,7 +3,8 @@
 $email_a = isset($_POST['email1']) ? $_POST('email1') : 'bluh_a'; //escape the string
 $email_b = isset($_POST['email2']) ? $_POST('email2') : 'bluh_b'; //escape the string
 //    var_dump($_POST);
-return json_encode(['message' => 'user exists', 'data' => [$email_a, $email_b]]);
+//return json_encode(['message' => 'user exists', 'data' => [$email_a, $email_b]]);
+echo "asdasdasdasAAAAAA";
 //    $query = "SELECT email FROM signup WHERE email='$email'";
 //    $result = mysqli_query($con, $query);
 //    $rows = mysqli_num_rows($result);
