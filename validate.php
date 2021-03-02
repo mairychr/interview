547d00b1e8d7331303b35c51cba67dbd41811b79
kostas o mastoras <kmitsios@agenso.gr>
2021-03-03 00:44:41 +0200

    debug 3

--- validate.php
+++ validate.php
@@ -1,17 +1,18 @@
 <?php
 //require('request_user.php');
-
-if (isset($_POST['email'])) { //change isSet to isset (it will not make any difference)
+if (isset($_REQUEST["email"])) { //change isSet to isset (it will not make any difference)
     $email = ($_POST['email']); //escape the string
 //    var_dump($_POST);
-    echo json_encode(['message' => 'user exists', 'data' => 11]);
-    $query = "SELECT email FROM signup WHERE email='$email'";
-    $result = mysqli_query($con, $query);
-    $rows = mysqli_num_rows($result);
-    if ($rows == 1) { //check rows greater then zero (although it will also not make any difference)
-        // echo '<font color="red">The email <strong>'.$email.'</strong>'. ' is already in use.</font>';
-        echo json_encode(['message' => 'user exists', 'data' => 11]);
-    } else {
-        echo json_encode(['message' => 'user does not exists', 'data' => 12]);
-    }
+    return json_encode(['message' => 'user exists', 'data' => 11]);
+//    $query = "SELECT email FROM signup WHERE email='$email'";
+//    $result = mysqli_query($con, $query);
+//    $rows = mysqli_num_rows($result);
+//    if ($rows == 1) { //check rows greater then zero (although it will also not make any difference)
+//        // echo '<font color="red">The email <strong>'.$email.'</strong>'. ' is already in use.</font>';
+//        echo json_encode(['message' => 'user exists', 'data' => 11]);
+//    } else {
+//        echo json_encode(['message' => 'user does not exists', 'data' => 12]);
+//    }
 }
+return json_encode(['message' => 'user exists', 'data' => 12]);
+?>
