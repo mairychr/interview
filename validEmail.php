85b48a238bafa8dc6461fffb9d2250410ae5c58b
kostas o mastoras <kmitsios@agenso.gr>
2021-03-03 02:10:01 +0200

    validation

--- validEmail.php
+++ validEmail.php
@@ -1,18 +1,17 @@
 <?php
-//require('request_user.php');
-$email_a = isset($_POST['email1']) ? $_POST('email1') : 'bluh_a'; //escape the string
-$email_b = isset($_POST['email2']) ? $_POST('email2') : 'bluh_b'; //escape the string
-//    var_dump($_POST);
-//return json_encode(['message' => 'user exists', 'data' => [$email_a, $email_b]]);
-echo "asdasdasdasAAAAAA";
-//    $query = "SELECT email FROM signup WHERE email='$email'";
-//    $result = mysqli_query($con, $query);
-//    $rows = mysqli_num_rows($result);
-//    if ($rows == 1) { //check rows greater then zero (although it will also not make any difference)
-//        // echo '<font color="red">The email <strong>'.$email.'</strong>'. ' is already in use.</font>';
-//        echo json_encode(['message' => 'user exists', 'data' => 11]);
-//    } else {
-//        echo json_encode(['message' => 'user does not exists', 'data' => 12]);
-//    }
-exit();
+require('db.php');
+$email = isset($_POST['email1']) ? $_POST('email1') : null;
+if ($email_a) {
+    $query = "SELECT email FROM signup WHERE email='$email'";
+    $result = mysqli_query($con, $query);
+    $rows = mysqli_num_rows($result);
+    if ($rows == 1) {
+        echo json_encode(['message' => 'user exists', 'data' => 11, 'status' => 409]);
+    } else {
+        echo json_encode(['message' => 'user does not exists', 'status' => 200]);
+    }
+
+} else {
+    return json_encode(['message' => 'invalid input', 'status' => 400]);
+}
 ?>
